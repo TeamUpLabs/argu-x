40f2d69d42fe5a5828b5ed4521ecc0d020bdc04d
LeeSeokmin <dltjrals13@naver.com>
2025-10-15 14:14:32 +0900

    Add Header component for SparringPage with dynamic debate display and link copying functionality; implement responsive design elements for improved user experience.

---
+++ src/components/sparring/header.tsx
@@ -0,0 +1,59 @@
+
+import { Debate } from "@/types/Debate";
+import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+import { Bookmark } from "lucide-react";
+
+interface HeaderProps {
+  debate?: Debate;
+  scale: number;
+  opacity: number;
+}
+
+export default function Header({ scale, debate, opacity }: HeaderProps) {
+  return (
+    <div className="flex items-center justify-between sticky bg-background top-25 z-10 pt-4 pb-1">
+      <div className={`flex items-center gap-4 origin-center origin-left`} style={{ transform: `scale(${scale})` }}>
+        {debate ? (
+          <>
+            <div className="bg-muted aspect-square rounded-md w-16 h-16 relative overflow-hidden">
+              <Image
+                src={debate.img}
+                alt={debate.title}
+                fill
+                sizes="80px"
+                className="object-cover"
+              />
+            </div>
+            <span className="text-foreground font-bold text-2xl">{debate.title}</span>
+          </>
+        ) : (
+          <>
+            <div className="bg-muted aspect-square rounded-md w-16 h-16">
+              {/* TODO: 이미지 */}
+            </div>
+            <span className="text-foreground font-bold text-2xl">토론 이름</span>
+          </>
+        )}
+      </div>
+      <div className="flex items-center gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Link />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>링크 복사</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="outline" size="icon">
+          <Bookmark />
+        </Button>
+      </div>
+      <div className="w-full h-px absolute bottom-0 left-0 bg-border" style={{ opacity: 1 - opacity }}></div>
+    </div>
+  );
+}
